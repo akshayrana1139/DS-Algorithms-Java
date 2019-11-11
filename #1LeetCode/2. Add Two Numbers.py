518e438ad260878e8534ac5b9ac633e9f7f8ad9e
Akshay Singh Rana <spawnakshay@gmail.com>
2019-11-11 02:07:57 -0500

    Create 2. Add Two Numbers.py

---
+++ #1LeetCode/2. Add Two Numbers.py
@@ -0,0 +1,39 @@
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Example:
+
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+# from typing import List 
+
+# Faster than 99% and less memory than 100%
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        r = ListNode(0)
+        l3 = r
+        carry = 0
+        while l1 or l2 or carry>0:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            
+            if l2:
+                sum+= l2.val
+                l2 = l2.next
+            
+            sum += carry
+            carry = int(sum/10)
+            l3.next = ListNode(sum%10)
+            l3 = l3.next # storing r as l3 keeps updating
+            
+        return r.next
