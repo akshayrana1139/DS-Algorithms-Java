4d54b341c340163f43201f17d4a851560bbf53cf
Akshay Singh Rana <spawnakshay@gmail.com>
2019-11-13 20:02:22 -0500

    practice

---
+++ #1LeetCode/0. Google Practice1.py
@@ -0,0 +1,27 @@
+import sys
+
+def solution(A):
+  """Your solution goes here."""
+  dic = {}; dic[1] = A[0]; rows = 1 
+  for index in range(1,len(A)):
+    val = A[index]
+    flag = True
+    for key in dic:
+      if dic[key] > val:
+        dic[key] = val
+        flag = False
+        break
+    if flag: 
+      rows += 1
+      dic[rows] = val
+  return rows
+
+def main():
+  """Read from stdin, solve the problem, write answer to stdout."""
+  input = sys.stdin.readline().split()
+  A = [int(x) for x in input[0].split(",")]
+  sys.stdout.write(str(solution(A)))
+
+
+if __name__ == "__main__":
+  main()
